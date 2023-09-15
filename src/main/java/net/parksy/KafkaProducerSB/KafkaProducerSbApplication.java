25fe9b83c746d6477e1323e59ed93829430e9e18
Darryl Parks <djparks@usa.net>
2023-09-15 13:14:57 -0500

    Add Spring Boot Initialized code for Kafka

---
+++ src/main/java/net/parksy/KafkaProducerSB/KafkaProducerSbApplication.java
@@ -0,0 +1,13 @@
+package net.parksy.KafkaProducerSB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/parksy/KafkaProducerSB/KafkaProducerSbApplicationTests.java
@@ -0,0 +1,13 @@
+package net.parksy.KafkaProducerSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
